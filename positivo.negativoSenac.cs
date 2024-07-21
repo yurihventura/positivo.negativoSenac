d16fa8e6dbd53dee3f55181d24b5af27e9cf5a0e
yurihventura <yuri_ven@hotmail.com>
2024-07-21 13:46:05 -0300

    positivoenegativo

---
+++ positivo.negativoSenac.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int num;
+        Console.WriteLine("Digite um número");
+        num = int.Parse(Console.ReadLine());
+        if (num >= 0)
+        {
+            Console.WriteLine("Número Positivo!");
+        }
+        else
+        {
+            Console.WriteLine("Número Negativo!");
+
+
+
+
+
+        }
+    }
+}
+
